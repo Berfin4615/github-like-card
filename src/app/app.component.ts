008f1c4178c8b9c806ee5b619cb2be9786e778cb
Berfin4615 <berfins.4615@gmail.com>
2024-06-18 05:20:54 +0300

    Card component mouse move event designed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GitHubCardComponent } from './git-hub-card/git-hub-card.component';
 
@@ -10,8 +10,33 @@ import { GitHubCardComponent } from './git-hub-card/git-hub-card.component';
     GitHubCardComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'github-like-card';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.executeJavaScript();
+  }
+
+  executeJavaScript() {
+    const cardContainer = document.querySelector(".glowing-card");
+    const card = document.querySelector(".glowing-card .card") as HTMLElement | null;
+
+    if (cardContainer && card) {
+      cardContainer.addEventListener("mousemove", (event: Event) => {
+        const e = event as MouseEvent;
+        const rect = card.getBoundingClientRect();
+        if (rect) {
+          const x = e.clientX - rect.left;
+          const y = e.clientY - rect.top;
+
+          card.style.setProperty("--x", `${x}px`);
+          card.style.setProperty("--y", `${y}px`);
+        }
+      });
+    }
+  }
 }
